7635dbf3599a5768afc3141ced7037735f888a82
gurezo <akihiko.kigure@gmail.com>
2018-03-21 12:15:25 +0900

    directory refactoring.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ExpressionsComponent } from './expressions/expressions.component';
-import { ClickComponent } from './click/click.component';
-import { PropertybindingComponent } from './propertybinding/propertybinding.component';
+import { ExpressionsComponent } from './components/expressions/expressions.component';
+import { ClickComponent } from './components/click/click.component';
+import { PropertybindingComponent } from './components/propertybinding/propertybinding.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/', pathMatch: 'full'},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 
 
 import { AppComponent } from './app.component';
-import { ExpressionsComponent } from './expressions/expressions.component';
-import { ClickComponent } from './click/click.component';
-import { PropertybindingComponent } from './propertybinding/propertybinding.component';
+import { ExpressionsComponent } from './components/expressions/expressions.component';
+import { ClickComponent } from './components/click/click.component';
+import { PropertybindingComponent } from './components/propertybinding/propertybinding.component';
 
 
 @NgModule({
